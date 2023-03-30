0d9f0d762212b4a078287f89a7b3028d0217e4ff
Eric Kuha <erickuha@gmail.com>
2023-03-30 14:29:34 -0500

    updating plugins

--- src/index.tsx
+++ src/index.tsx
@@ -1,58 +1,63 @@
 import * as esbuild from 'esbuild-wasm';
-import {useEffect, useRef, useState} from "react";
+import React, { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom/client';
-import React from 'react';
-import {unpkgPathPlugin} from "./plugins/unpkg-path-plugin";
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from "./plugins/fetch-plugin";
 
 const App = () => {
     const ref = useRef<any>();
     const [input, setInput] = useState('');
     const [code, setCode] = useState('');
+    const env:string = ['process', 'env', 'NODE_ENV'].join('.');
 
-    const startService = async() => {
+    const startService = async () => {
         ref.current = await esbuild.startService({
             worker: true,
-            wasmURL: '/esbuild.wasm'
+            wasmURL: '/esbuild.wasm',
         });
     };
-
     useEffect(() => {
         startService();
     }, []);
 
     const onClick = async () => {
-        if(!ref.current) {
+        if (!ref.current) {
             return;
         }
+
         const result = await ref.current.build({
             entryPoints: ['index.js'],
             bundle: true,
             write: false,
-            plugins: [unpkgPathPlugin()],
+            plugins: [
+                unpkgPathPlugin(),
+                fetchPlugin(input)
+            ],
             define: {
-                'proccess.env.NODE_ENV': '"production"',
-                global: 'window',
-            }
+                [env]: '"production"',
+                globalName: 'window',
+            },
         });
-
-        // console.log(result);
 
         setCode(result.outputFiles[0].text);
     };
 
-    return <>
-        <textarea
-            onChange={e => setInput(e.target.value)}
-        ></textarea>
+    return (
         <div>
-            <button onClick={onClick}>Submit</button>
+      <textarea
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+            <div>
+                <button onClick={onClick}>Submit</button>
+            </div>
+            <pre>{code}</pre>
         </div>
-        <pre>{code}</pre>
-    </>;
+    );
 };
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <App />
+        <App/>
     </React.StrictMode>
-)
+);
