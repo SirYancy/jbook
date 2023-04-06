5ae8e732a650a1bd0fbcd98528fc2cdfa6ee571e
Eric Kuha <erickuha@gmail.com>
2023-04-06 13:47:43 -0500

    refactor and organize code

--- src/index.tsx
+++ src/index.tsx
@@ -1,73 +1,17 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import * as esbuild from 'esbuild-wasm';
-import React, { useState, useEffect, useRef } from 'react';
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
-import { fetchPlugin } from "./plugins/fetch-plugin";
+import React, { useState } from 'react';
 import CodeEditor from "./components/code-editor";
+import Preview from "./components/preview";
+import bundle from './bundler';
 
 const App = () => {
-    const ref = useRef<any>();
-    const iframe = useRef<any>();
+    const [code, setCode] = useState('');
     const [input, setInput] = useState<string | undefined>('');
-    const env:string = ['process', 'env', 'NODE_ENV'].join('.');
-
-    const startService = async () => {
-        ref.current = await esbuild.startService({
-            worker: true,
-            wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
-        });
-    };
-    useEffect(() => {
-        startService();
-    }, []);
 
     const onClick = async () => {
-        if (!ref.current) {
-            return;
-        }
-
-        iframe.current.srcdoc = html;
-
-        const result = await ref.current.build({
-            entryPoints: ['index.js'],
-            bundle: true,
-            write: false,
-            plugins: [
-                unpkgPathPlugin(),
-                fetchPlugin(input)
-            ],
-            define: {
-                [env]: '"production"',
-                globalName: 'window',
-            },
-        });
-
-        iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
-
+        const output = await bundle(input);
+        setCode(output);
     };
-
-    const html = `
-        <html>
-        <head></head>
-        <body>
-            <div id="root"></div>
-            <script>
-                window.addEventListener('message', (event) => {
-                    try{
-                        eval(event.data);
-                    } catch (err) {
-                        const root = document.querySelector('#root');
-                        root.innerHTML = 
-                        '<div style="color: red;"><h4>Runtime Error</h4>' + 
-                         err +
-                         '</div>';
-                        console.error(err);
-                    }
-                }, false);
-            </script>
-        </body>
-        </html>
-    `;
 
     return (
         <div>
@@ -75,18 +19,10 @@ const App = () => {
                 initialValue="const a = 1;"
                 onChange={(value) => setInput(value)}
             />
-      <textarea
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-      ></textarea>
             <div>
                 <button onClick={onClick}>Submit</button>
             </div>
-            <iframe title="preview"
-                    ref={iframe}
-                    sandbox="allow-scripts"
-                    srcDoc={html}
-            />
+            <Preview code={code} />
         </div>
     );
 };
