2433cf06d83e79d8593dbcbab5e6d33384e92aea
Eric Kuha <erickuha@gmail.com>
2023-04-11 12:02:17 -0500

    resizing is looking great

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,5 +1,6 @@
 import './resizable.css';
-import {ResizableBox} from "react-resizable";
+import { useEffect, useState } from "react";
+import {ResizableBox, ResizableBoxProps} from "react-resizable";
 import React from "react";
 
 interface ResizableProps {
@@ -8,12 +9,49 @@ interface ResizableProps {
 }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children}) => {
+    let resizableProps: ResizableBoxProps;
+    const [innerHeight, setInnerHeight] = useState(window.innerHeight);
+    const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        let timer: any;
+        const listener = () => {
+            if (timer) {
+                clearTimeout(timer);
+            }
+            timer = setTimeout(() => {
+                setInnerHeight(window.innerHeight);
+                setInnerWidth(window.innerWidth);
+            }, 100);
+        };
+        window.addEventListener('resize', listener);
+
+        return () => {
+            window.removeEventListener('resize', listener);
+        };
+    }, []);
+
+    if (direction === 'horizontal'){
+        resizableProps = {
+            className: 'resize-horizontal',
+            minConstraints: [innerWidth * 0.2, Infinity],
+            maxConstraints: [innerWidth * 0.75, Infinity],
+            height: Infinity,
+            width: innerWidth * 0.75,
+            resizeHandles: ['e'],
+        };
+    } else {
+        resizableProps = {
+            minConstraints: [Infinity, 24],
+            maxConstraints: [Infinity, innerHeight * 0.9],
+            height: 300,
+            width: Infinity,
+            resizeHandles: ['s'],
+        };
+    }
+
     return (
-        <ResizableBox
-            height={300}
-            width={Infinity}
-            resizeHandles={['s']}
-        >
+        <ResizableBox {...resizableProps}>
             { children }
         </ResizableBox>
     );
